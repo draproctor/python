0842fea8208d2f3dc8675b39dbb665a6c9b592b2
Austin Proctor <drproctor@fb.com>
2018-03-17 11:16:20 -0700

    Updated to python3. Updated to 2018 tax brackets.

--- tax_calc.py
+++ tax_calc.py
@@ -1,57 +1,70 @@
-def tax_calc(income):
+def income_remaining(income):
+    # Convert to float for proper calculations
     income = float(income)
-    first_bracket = income - (0.10 * income)
-    second_bracket = income - (932 + 0.15 * (income - 9325))
-    third_bracket = income - (5226 + 0.25 * (income - 37950))
-    fourth_bracket = income - (18713 + 0.28 * (income - 91900))
-    fifth_bracket = income - (46643 + 0.33 * (income - 191650))
-    sixth_bracket = income - (120910 + 0.35 * (income - 416700))
-    seventh_bracket = income - (121505 + 0.396 * (income - 418400))
-    if income < 9325:
-        return first_bracket
-    elif income > 9326 and income < 37950:
-        return second_bracket
-    elif income > 37951 and income < 91900:
-        return third_bracket
-    elif income > 91901 and income < 191650:
-        return fourth_bracket
-    elif income > 191651 and income < 416700:
-        return fifth_bracket
-    elif income > 416701 and income < 418400:
-        return sixth_bracket
-    elif income > 418400:
-        return seventh_bracket
+
+    # First tax bracket
+    if income <= 9525:
+        return income - (0.10 * income)
+    # Second tax bracket
+    elif income > 9326 and income <= 38700:
+        return income - (952.50 + 0.12 * (income - 9525))
+    # Third tax bracket
+    elif income > 38701 and income <= 82500:
+        return income - (4453.50 + 0.22 * (income - 38700))
+    # Fourth tax bracket
+    elif income > 82501 and income <= 157500:
+        return income - (14089.50 + 0.24 * (income - 82500))
+    # Fifth tax bracket
+    elif income > 157501 and income <= 200000:
+        return income - (32089.50 + 0.32 * (income - 157500))
+    # Sixth tax bracket
+    elif income > 200001 and income <= 500000:
+        return income - (45689.50 + 0.35 * (income - 200000))
+    # Seventh tax bracket
+    elif income > 500001:
+        return income - (150689.50 + 0.37 * (income - 500000))
     else:
-        print 'Invalid integer entered. Please try again.'
+        print('Invalid value entered. Please try again.')
+
+
+def tax_bonuses(bonus):
+    return float(bonus) / 2
+
 
 def paycheck_info(post_tax):
     return post_tax / 26
 
-def monthly_info(post_tax):
+
+def info_monthly(post_tax):
     return post_tax / 12
 
-def max_rent_info(monthly):
+
+def info_max_rent(monthly):
     return monthly / 3
 
-def mo_post_rent(monthly,rent):
+
+def mo_post_rent(monthly, rent):
     return monthly - rent
 
-def mo_all_bills(monthly,rent,bills):
+
+def mo_all_bills(monthly, rent, bills):
     return monthly - rent - bills
 
-income = float(raw_input("What's your yearly income?\n"))
-rent = float(raw_input("What's your current monthly rent?\n"))
-bills = float(raw_input("What's your current misc. bills per month?\n"))
 
-post_tax = tax_calc(income)
+income = input('What is your yearly income?\n')
+bonus = input('How much did you earn in bonuses for the year?\n')
+rent = float(input('What is your current monthly rent?\n'))
+bills = float(input('What is your current misc. bills per month?\n'))
+
+post_tax = income_remaining(income) + tax_bonuses(bonus)
 pc = paycheck_info(post_tax)
-mi = monthly_info(post_tax)
-mri = max_rent_info(mi)
+mi = info_monthly(post_tax)
+mri = info_max_rent(mi)
 mpr = mo_post_rent(mi, rent)
 mab = mo_all_bills(mi, rent, bills)
 
-print "%f is earned each year after taxes." % (post_tax)
-print "%f is the maximum rent that can be paid." % (mri)
-print "%f is earned per paycheck." % (pc)
-print "%f is earned per month." % (mi)
-print "%f is earned per month after all bills." % (mpr)
+print('%f is earned each year after taxes.' % (round(post_tax, 2)))
+print('%f is the maximum rent that can be paid.' % (round(mri, 2)))
+print('%f is earned per paycheck.' % (round(pc, 2)))
+print('%f is earned per month.' % (round(mi, 2)))
+print('%f is earned per month after all bills.' % (round(mpr, 2)))
