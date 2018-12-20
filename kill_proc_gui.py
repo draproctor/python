737217a0e7164ef33590e0c6b14feebda5ce510c
Austin Proctor <ajhproctor@gmail.com>
2018-12-20 11:07:19 -0800

    Apply black

--- kill_proc_gui.py
+++ kill_proc_gui.py
@@ -1,5 +1,5 @@
 #!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3.7
-'''GUI tool for killing stuck processes'''
+"""GUI tool for killing stuck processes"""
 
 from tkinter import Tk, Label, Button
 from subprocess import call
@@ -10,33 +10,27 @@ class PkillGui:
         self.master = master
         master.title("Kill 'em dead!")
 
-        self.label = Label(master, text='Click a button to murdile a process.')
+        self.label = Label(master, text="Click a button to murdile a process.")
         self.label.pack()
 
         # Buttons region
         self.kill_citrix = Button(
-            master,
-            text='Kill BadApp',
-            command=lambda: self.kill_process('BadApp'),
+            master, text="Kill BadApp", command=lambda: self.kill_process("BadApp")
         )
         self.kill_citrix.pack()
 
         self.kill_bomgar = Button(
             master,
-            text='Kill CantStopWontStop',
-            command=lambda: self.kill_process('CantStopWontStop'),
+            text="Kill CantStopWontStop",
+            command=lambda: self.kill_process("CantStopWontStop"),
         )
         self.kill_bomgar.pack()
 
-        self.quit = Button(
-            master,
-            text='Quit',
-            command=master.quit
-        )
+        self.quit = Button(master, text="Quit", command=master.quit)
         self.quit.pack()
 
     def kill_process(self, process: str):
-        call(['/usr/bin/pkill', '-i', process])
+        call(["/usr/bin/pkill", "-i", process])
 
 
 root = Tk()
