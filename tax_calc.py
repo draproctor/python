4b16f7110a3716990cc2141631a1ffa845705bf3
Austin Proctor <ajhproctor@gmail.com>
2018-12-20 11:03:46 -0800

    Apply black formatter

--- tax_calc.py
+++ tax_calc.py
@@ -12,19 +12,19 @@ def income_remaining(income):
     elif income > 38701 and income <= 82500:
         return income - (4453.50 + 0.22 * (income - 38700))
     # Fourth tax bracket
-    elif income > 82501 and income <= 157500:
+    elif income > 82501 and income <= 157_500:
         return income - (14089.50 + 0.24 * (income - 82500))
     # Fifth tax bracket
-    elif income > 157501 and income <= 200000:
-        return income - (32089.50 + 0.32 * (income - 157500))
+    elif income > 157_501 and income <= 200_000:
+        return income - (32089.50 + 0.32 * (income - 157_500))
     # Sixth tax bracket
-    elif income > 200001 and income <= 500000:
-        return income - (45689.50 + 0.35 * (income - 200000))
+    elif income > 200_001 and income <= 500_000:
+        return income - (45689.50 + 0.35 * (income - 200_000))
     # Seventh tax bracket
-    elif income > 500001:
-        return income - (150689.50 + 0.37 * (income - 500000))
+    elif income > 500_001:
+        return income - (150_689.50 + 0.37 * (income - 500_000))
     else:
-        print('Invalid value entered. Please try again.')
+        print("Invalid value entered. Please try again.")
 
 
 def tax_bonuses(bonus):
@@ -51,10 +51,10 @@ def mo_all_bills(monthly, rent, bills):
     return monthly - rent - bills
 
 
-income = input('What is your yearly income?\n')
-bonus = input('How much did you earn in bonuses for the year?\n')
-rent = float(input('What is your current monthly rent?\n'))
-bills = float(input('What is your current misc. bills per month?\n'))
+income = input("What is your yearly income?\n")
+bonus = input("How much did you earn in bonuses for the year?\n")
+rent = float(input("What is your current monthly rent?\n"))
+bills = float(input("What is your current misc. bills per month?\n"))
 
 post_tax = income_remaining(income) + tax_bonuses(bonus)
 pc = paycheck_info(post_tax)
@@ -63,8 +63,8 @@ mri = info_max_rent(mi)
 mpr = mo_post_rent(mi, rent)
 mab = mo_all_bills(mi, rent, bills)
 
-print('%f is earned each year after taxes.' % (round(post_tax, 2)))
-print('%f is the maximum rent that can be paid.' % (round(mri, 2)))
-print('%f is earned per paycheck.' % (round(pc, 2)))
-print('%f is earned per month.' % (round(mi, 2)))
-print('%f is earned per month after all bills.' % (round(mpr, 2)))
+print(f"{round(post_tax, 2)} is earned each year after taxes.")
+print(f"{round(mri, 2)} is the maximum rent that can be paid.")
+print(f"{round(pc, 2)} is earned per paycheck.")
+print(f"{round(mi, 2)} is earned per month.")
+print(f"{round(mpr, 2)} is earned per month after all bills.")
