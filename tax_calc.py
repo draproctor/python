3a192590cdf170a5baea02a3cb9742f24e72945d
Austin Proctor <ajhproctor@gmail.com>
2024-06-14 16:49:26 -0500

    Update tax_calc for 2024, refactor to be less clowny

--- tax_calc.py
+++ tax_calc.py
@@ -1,53 +1,60 @@
 #!/usr/bin/env python3
 
 import sys
+from dataclasses import dataclass
+
+import click
 from tabulate import tabulate
 
 
-try:
-    taxable_income = int(sys.argv[1])
-except ValueError:
-    print(f"{sys.argv[1]} is not a valid number for calculating a taxable income.")
-    exit(1)
+@dataclass
+class TaxBracket:
+    rate: float
+    minimum: int
+    maximum: int
+    bonus_tax: int | float = 0
 
-# single filer brackets from https://www.nerdwallet.com/article/taxes/federal-income-tax-brackets
-bracket_limits = {
-    (518_401, sys.maxsize): lambda x: 156235 + (0.37 * (x - 518400)),
-    (207_351, 518_400): lambda x: 47367.5 + (0.35 * (x - 207350)),
-    (163_301, 207_350): lambda x: 33271.5 + (0.32 * (x - 163300)),
-    (85_526, 163_300): lambda x: 14605.5 + (0.24 * (x - 85525)),
-    (40_126, 85_525): lambda x: 4617.5 + (0.22 * (x - 40125)),
-    (9876, 40_125): lambda x: 987.5 + (0.12 * (x - 9875)),
-    (0, 9875): lambda x: 0.10 * x,
-}
+    def tax_owed(self, income: float | int) -> float:
+        if income <= self.minimum:
+            return 0.0
+        income = min(income, self.maximum)
+        owed = self.rate * (income - self.minimum - 1) + self.bonus_tax
+        print(f"{self.rate} * ({income} - {self.minimum}) = ${owed:,.2f}")
+        return owed
 
-selected_bracket = None
 
-for income_bounds, tax_bracket_calc in bracket_limits.items():
-    lower_bound = income_bounds[0]
-    upper_bound = income_bounds[1]
-    if lower_bound < taxable_income and taxable_income < upper_bound:
-        selected_bracket = tax_bracket_calc
-        break
+@click.command()
+@click.argument("taxable_income", type=float)
+def main(taxable_income: float) -> None:
+    tax_brackets = [
+        TaxBracket(rate=0.10, minimum=0, maximum=11_600, bonus_tax=0),
+        TaxBracket(rate=0.12, minimum=11_601, maximum=47_150, bonus_tax=0),
+        TaxBracket(rate=0.22, minimum=47_151, maximum=100_525, bonus_tax=0),
+        TaxBracket(rate=0.24, minimum=100_526, maximum=191_950, bonus_tax=0),
+        TaxBracket(rate=0.32, minimum=191_951, maximum=243_725, bonus_tax=0),
+        TaxBracket(rate=0.35, minimum=243_726, maximum=609_350, bonus_tax=0),
+        TaxBracket(
+            rate=0.37, minimum=609_351, maximum=sys.maxsize, bonus_tax=183_647.25
+        ),
+    ]
 
-if selected_bracket == None:
-    print(f"No tax bracket satisfied the range. What the hell is {taxable_income}?")
-    exit(1)
+    taxes_owed = sum(bracket.tax_owed(taxable_income) for bracket in tax_brackets)
 
-tax_owed = selected_bracket(taxable_income)
-gross_yearly_income = taxable_income - tax_owed
+    tax_values = {
+        "Income": taxable_income,
+        "Tax Owed": taxes_owed,
+        "Gross Income": taxable_income - taxes_owed,
+        "Pct. Lost to Taxes": "{0:.0%}".format(taxes_owed / taxable_income),
+    }
+    print(tabulate([tax_values], headers="keys", tablefmt="github"), "\n")
 
-tax_values = {
-    "Income": taxable_income,
-    "Tax Owed": selected_bracket(taxable_income),
-    "Gross Income": gross_yearly_income,
-    "Pct. Lost to Taxes": "{0:.0%}".format(tax_owed / taxable_income),
-}
-print(tabulate([tax_values], headers="keys", tablefmt="github"), "\n")
+    spendable_monies = {
+        "Monthly Gross Income": tax_values["Gross Income"] / 12,
+        "Paycheck Value": tax_values["Gross Income"] / 26,
+        "Maximum Rent": tax_values["Gross Income"] / 36,
+    }
+    print(tabulate([spendable_monies], headers="keys", tablefmt="github"))
 
-spendable_monies = {
-    "Monthly Gross Income": gross_yearly_income / 12,
-    "Paycheck Value": gross_yearly_income / 26,
-    "Maximum Rent": gross_yearly_income / 36,
-}
-print(tabulate([spendable_monies], headers="keys", tablefmt="github"))
+
+if __name__ == "__main__":
+    main()
